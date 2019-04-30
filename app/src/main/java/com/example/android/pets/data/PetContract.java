847d3dedcfc3a314b009f09139293314a1463bbd
Jayden <JaydenMcPhedrain@gmail.com>
2019-04-29 20:21:48 -0700

    Created a helper class to create the pets database.

---
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -0,0 +1,26 @@
+package com.example.android.pets.data;
+
+import android.provider.BaseColumns;
+
+public final class PetContract {
+
+    public static abstract class PetEntry implements BaseColumns {
+
+        public final static String TABLE_NAME = "pets";
+
+        public final static String _ID = BaseColumns._ID;
+        public final static String PET_NAME = "name";
+        public final static String PET_BREED = "breed";
+        public final static String PET_GENDER = "gender";
+        public final static String PET_WEIGHT = "weight";
+
+
+        public final static int GENDER_UNKNOWN = 0;
+        public final static int GENDER_MALE = 1;
+        public final static int GENDER_FEMALE = 2;
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -0,0 +1,34 @@
+package com.example.android.pets.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.android.pets.data.PetContract.PetEntry;
+
+public class PetDbHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "shelter.db";
+
+    public PetDbHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        String SQL_CREATE_PETS_TABLE = "CREATE TABLE "  + PetEntry.TABLE_NAME + "("
+                                                        + PetEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                                                        + PetEntry.PET_NAME + " TEXT NOT NULL, "
+                                                        + PetEntry.PET_BREED + " TEXT, "
+                                                        + PetEntry.PET_GENDER + " INTEGER NOT NULL, "
+                                                        + PetEntry.PET_WEIGHT + " INTEGER NOT NULL DEFAULT 0);";
+        db.execSQL(SQL_CREATE_PETS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1){
+
+    }
+
+
+}
